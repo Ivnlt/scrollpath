254039b27ea782027884648bffa4f5bb033ac964
Iván Lutin <ivanlutin@gmail.com>
2014-07-31 10:20:47 -0600

    pablo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(init)
 
+function init(){
+
+
+	$.fn.scrollPath("getPath")
+
+	.moveTo(400, 400, {name:"seccion1"})
+
+	.lineTo(1040, 400)
+	.lineTo(1040, 1120, {name:"seccion2"})
+
+	$("#container").scrollPath({
+			drawPath:true,
+			scrollBar:true,
+			wrapAround:true,
+
+		});
+}
